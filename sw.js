71a75fd167b8d70bb54cf8b80ee40ef3c6c9bfa0
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-11-05 20:23:19 -0700

    sw done

--- sw.js
+++ sw.js
@@ -4,11 +4,29 @@
 const CACHE_NAME = 'lab-7-starter';
 
 // Once the service worker has been installed, feed it some initial URLs to cache
+const urlsToCache =[
+  '/',
+  '/assets/styles/main.css',
+  '/index.html',
+  '/favicon.ico',
+  '/assets/scripts/main.js',
+  '/assets/scripts/Router.js',
+  '/assets/components/RecipeCard.js',
+  '/assets/components/RecipeExpand.js',
+  '/assets/images/icons/'
+]
 self.addEventListener('install', function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache)=>{
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +39,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +48,36 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request)
+      .then((response)=>{
+        // check if found in cache
+        if (response) {
+          return response;
+        }
+
+        // otherwise we attempt to fetch
+        return fetch(event.request)
+          .then((response)=>{
+            // check validity
+            if (!response || response.status !== 200) {
+              // invalid, let whatever fetched deal with it
+              return response;
+            }
+
+            // is valid: clone, cache, and return
+
+            // clone
+            let responseClone = response.clone();
+            // cache the clone
+            caches.open(CACHE_NAME)
+              .then((cache)=> {
+                cache.put(event.request, responseClone);
+              })
+
+            // return the original response
+            return response;
+          });
+      })
+  );
 });
